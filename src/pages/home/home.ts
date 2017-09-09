34a6fe038664494c40c9d523f7e85a39f7777d5f
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2017-09-09 12:45:26 -0300

    Button to customers added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CustomersPage } from '../customers/customers';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController) {
 
-  }
+    }
 
+    goToCustomers() {
+        //push another page onto the history stack
+        //causing the nav controller to animate the new page in
+        this.navCtrl.push(CustomersPage);
+    }
 }
