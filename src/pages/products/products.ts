48bfa4031d0417920c38108c56804eaf038d076f
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2017-11-17 16:15:23 -0200

    Product list update

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,6 +1,6 @@
-import {Component} from '@angular/core';
-import {IonicPage, NavController, NavParams} from 'ionic-angular';
-import {ApiProvider} from "../../providers/api/api";
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+import { ApiProvider } from '../../providers/api/api';
 
 /**
  * Generated class for the ProductsPage page.
@@ -15,21 +15,14 @@ import {ApiProvider} from "../../providers/api/api";
     templateUrl: 'products.html',
 })
 export class ProductsPage {
+
     public products;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, private apiProvider: ApiProvider) {
+    constructor(private apiProvider: ApiProvider) {
     }
 
     ionViewDidLoad() {
-        console.log('ionViewDidLoad OrdersPage');
-
-        this.initializeItems();
-    }
-
-    /**
-     * Reset items back to all of the items
-     */
-    initializeItems() {
         this.apiProvider.builder('products').loader().get().then((res) => this.products = res);
     }
+
 }
