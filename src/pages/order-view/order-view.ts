840aa21edf2bbaaeae27eb1684fbe1d3139a8f72
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2018-01-01 17:38:59 -0200

    New line removed

--- src/pages/order-view/order-view.ts
+++ src/pages/order-view/order-view.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ApiProvider } from '../../providers/api/api';
+import { ListFormPage } from "../list-form/list-form";
 
 /**
  * Generated class for the OrderViewPage page.
@@ -15,7 +16,6 @@ import { ApiProvider } from '../../providers/api/api';
     templateUrl: 'order-view.html',
 })
 export class OrderViewPage {
-
     public order;
 
     constructor(public navCtrl: NavController, public navParams: NavParams, private apiProvider: ApiProvider) {
@@ -30,5 +30,12 @@ export class OrderViewPage {
      */
     loadItem(id) {
         this.apiProvider.builder('orders/' + id).loader().get().then((res) => this.order = res);
+    }
+
+    /**
+     * Push to list form page
+     */
+    goToForm() {
+        this.navCtrl.push(ListFormPage, {customer: this.order.customer.id, order: this.order.order_product});
     }
 }
