34b697a3cd570f7e1488f96029f20dc7495a9d23
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2018-01-26 03:55:44 -0200

    Login/authentication improved and general layout fixes

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { ApiProvider } from "../api/api";
-import { Http } from "@angular/http";
 import 'rxjs/add/operator/toPromise';
 
 /*
@@ -11,13 +10,59 @@ import 'rxjs/add/operator/toPromise';
 */
 @Injectable()
 export class AuthProvider extends ApiProvider {
-    getUser(token) {
-        return this.builder('auth/me', token).loader().get()
-            .then((res) => res);
+    client = {
+        client_id: '5',
+        client_secret: '1a8RPbjkYzOTMImXYlcla4Ra7Q08sZvpikEs1ZNk',
+        scope: ''
+    };
+
+    /**
+     *
+     * @returns {Promise<T | any[]>}
+     */
+    getUser() {
+        return this.builder('auth/me').loader().get();
     }
 
+    /**
+     *
+     * @param data
+     * @returns {Promise<T | any[]>}
+     */
     login(data) {
-        return this.http.post(this.urlBase + 'oauth/token', data).toPromise()
-            .then((res) => res.json() || {});
+        console.log('login');
+
+        data = this.buildAuthForm(data);
+        data.grant_type = 'password';
+
+        return this.http.post(this.urlBase + 'oauth/token', data).toPromise().then((res) => res.json());
+    }
+
+    /**
+     *
+     * @param data
+     * @returns {Promise<T | any[]>}
+     */
+    refreshToken(data) {
+        console.log('refresh');
+
+        data = this.buildAuthForm(data);
+        data.grant_type = 'refresh_token';
+
+        let observable = this.http.post(this.urlBase + 'oauth/token', data);
+        return this.toPromise(observable);
+    }
+
+    /**
+     *
+     * @param data
+     * @returns {any}
+     */
+    private buildAuthForm(data) {
+        data.client_id = this.client.client_id;
+        data.client_secret = this.client.client_secret;
+        data.scope = this.client.scope;
+
+        return data;
     }
 }
