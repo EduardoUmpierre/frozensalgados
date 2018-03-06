7ec8628401a2416caf185dabfaebedc89467689e
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2018-03-06 00:08:45 -0300

    Products list options added

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
+import { ActionSheetController, AlertController, IonicPage, NavController } from 'ionic-angular';
 import { ApiProvider } from '../../providers/api/api';
+import { Storage } from "@ionic/storage";
+import { CustomerFormPage } from "../customer-form/customer-form";
 
 /**
  * Generated class for the ProductsPage page.
@@ -15,14 +17,80 @@ import { ApiProvider } from '../../providers/api/api';
     templateUrl: 'products.html',
 })
 export class ProductsPage {
+    user: any = {};
+    products = [];
 
-    public products;
-
-    constructor(private apiProvider: ApiProvider) {
+    constructor(public navCtrl: NavController, private apiProvider: ApiProvider, public storage: Storage, public actionSheetCtrl: ActionSheetController, private alertCtrl: AlertController) {
     }
 
     ionViewDidLoad() {
+        this.storage.get('user').then((user) => this.user = user);
         this.apiProvider.builder('products').loader().get().subscribe((res) => this.products = res);
     }
 
+    /**
+     * Push to customer form page
+     *
+     * @param {number} id
+     */
+    goToForm(id: number = null) {
+        this.navCtrl.push(CustomerFormPage, {id: id});
+    }
+
+    /**
+     * @param {number} id
+     * @param {number} key
+     */
+    showOptions(id: number, key: number) {
+        let actionSheet = this.actionSheetCtrl.create({
+            title: 'Opções',
+            buttons: [
+                {
+                    text: 'Editar',
+                    handler: () => this.goToForm(id)
+                },
+                {
+                    text: 'Excluir',
+                    role: 'destructive',
+                    handler: () => {
+                        let alert = this.alertCtrl.create({
+                            title: 'Confirmar exclusão',
+                            message: 'Deseja remover esse produto?',
+                            buttons: [
+                                {
+                                    text: 'Cancelar',
+                                    role: 'cancel'
+                                },
+                                {
+                                    text: 'Remover',
+                                    handler: () => {
+                                        this.apiProvider.builder('products/' + id).loader().delete().subscribe((res) => this.remove(key));
+                                    }
+                                }
+                            ]
+                        });
+
+                        alert.present();
+                    }
+                },
+                {
+                    text: 'Cancelar',
+                    role: 'cancel'
+                }
+            ]
+        });
+
+        actionSheet.present();
+    }
+
+    /**
+     * Removes a order item
+     *
+     * @param {number} key
+     */
+    remove(key: number) {
+        if (this.products[key]) {
+            this.products.splice(key, 1);
+        }
+    }
 }
