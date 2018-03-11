a7eefc938e66a013170305c0776a40f8646f0e37
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2018-03-11 19:18:31 -0300

    Product form validation added

--- src/pages/products/form/product-form.ts
+++ src/pages/products/form/product-form.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ApiProvider } from "../../../providers/api/api";
-import { ProductsPage } from "../../products/index/products";
+import { ProductsPage } from "../index/products";
+import { Validators, FormBuilder, FormGroup, FormControl } from "@angular/forms";
 
 /**
  * Generated class for the ProductFormPage page.
@@ -16,18 +17,31 @@ import { ProductsPage } from "../../products/index/products";
     templateUrl: 'product-form.html',
 })
 export class ProductFormPage {
-    pageTitle = 'Novo produto';
-    product = {};
+    private pageTitle = 'Novo produto';
+    private form: FormGroup;
+    private id: number = null;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, private apiProvider: ApiProvider) {
+    constructor(public navCtrl: NavController, public navParams: NavParams, private apiProvider: ApiProvider, private formBuilder: FormBuilder) {
         if (navParams.get('id')) {
             this.pageTitle = 'Editar produto';
+            this.id = navParams.get('id');
         }
+
+        this.form = this.formBuilder.group({
+            name: new FormControl('', Validators.required),
+            price: new FormControl('', Validators.required)
+        });
     }
 
+    /**
+     *
+     */
     ionViewDidLoad() {
-        if (this.navParams.get('id')) {
-            this.apiProvider.builder('products/' + this.navParams.get('id')).loader().get().subscribe((res) => this.product = res);
+        if (this.id) {
+            this.apiProvider.builder('products/' + this.id).loader().get().subscribe(res => {
+                this.form.controls['name'].setValue(res.name);
+                this.form.controls['price'].setValue(res.price);
+            });
         }
     }
 
@@ -35,13 +49,16 @@ export class ProductFormPage {
      * Submit function
      */
     submit() {
-        if (this.navParams.get('id')) {
-            this.apiProvider.builder('products/' + this.navParams.get('id')).loader().put(this.product).subscribe((res) => this.redirect());
+        if (this.id) {
+            this.apiProvider.builder('products/' + this.id).loader().put(Object.assign({}, {id: this.id}, this.form.value)).subscribe(res => this.redirect());
         } else {
-            this.apiProvider.builder('products').loader().post(this.product).subscribe((res) => this.redirect());
+            this.apiProvider.builder('products').loader().post(this.form.value).subscribe(res => this.redirect());
         }
     }
 
+    /**
+     *
+     */
     redirect() {
         this.navCtrl.push(ProductsPage).then(() => {
             this.navCtrl.remove(this.navCtrl.getActive().index - 2, 2);
